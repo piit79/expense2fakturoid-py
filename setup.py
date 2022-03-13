560de640103313103cdd2f62b1413cf330db391a
Petr Sedlacek <petr@sedlacek.biz>
2022-03-13 22:16:04 +0100

    Add setup.py; bump version to 1.0.1

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+long_description = """
+Allows creating complete expense entries in the on-line invoicing system Fakturoid <https://www.fakturoid.cz/>
+by parsing the PDF invoices/bills.
+"""
+
+setup(
+    name='expense2fakturoid',
+    version='1.0.1',
+    packages=['expense2fakturoid', 'expense2fakturoid.parsers'],
+    url='https://github.com/piit79/expense2fakturoid-py',
+    license='GPLv3',
+    author='Petr Sedlacek',
+    author_email='petr@sedlacek.biz',
+    description='Import expenses from PDF to Fakturoid',
+    long_description=long_description,
+    platforms='any',
+    keywords=['fakturoid', 'expense', 'import'],
+    install_requires=['pdftotext', 'fakturoid'],
+    # tests_require=['mock'],  # No tests yet, sorry
+    # test_suite="tests",
+    scripts=['bin/expense2fakturoid'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Office/Business :: Financial :: Accounting",
+    ],
+)
