86e647a909eb764e504fd8c73db3ca4765565160
Petr Sedlacek <petr@sedlacek.biz>
2022-03-13 22:15:41 +0100

    Refactor into proper module structure

--- expense2fakturoid/__init__.py
+++ expense2fakturoid/__init__.py
@@ -1,12 +1,8 @@
-import argparse
-import sys
-import yaml
 from typing import Any, Dict, List, Optional, Type
 
 from fakturoid import Fakturoid, Expense, InvoiceLine, Subject
-from requests.exceptions import HTTPError
 
-from parsers import *
+from .parsers import *
 
 
 class InvalidConfigException(Exception):
@@ -37,10 +33,10 @@ class Suppliers:
         return None
 
 
-class Expenses2Fakturoid:
+class Expense2Fakturoid:
     APP_NAME = 'Expense2Fakturoid'
 
-    def __init__(self, config, supplier_code, filename):
+    def __init__(self, config: Dict[str, Any], supplier_code: str, filename: str):
         self.fa = Fakturoid(config.get('slug'), config.get('email'), config.get('api_key'), self.APP_NAME)
         self.config = config
         self.supplier_code = supplier_code
@@ -96,68 +92,3 @@ class Expenses2Fakturoid:
             self.mark_expense_paid(expense, paid_on=data['issued_on'])
 
         return expense.html_url
-
-
-def get_args():
-    """
-    Parse and return the command-line arguments
-    """
-    parser = argparse.ArgumentParser(description='Parse supplier invoice and create it in Fakturoid')
-    parser.add_argument('--config', '-c', help='config file name', default='expense2fakturoid.yaml')
-    parser.add_argument('--supplier', '-s', help='supplier type', required=True, choices=Suppliers.get_valid_codes())
-    parser.add_argument('filename', help='supplier invoice file name')
-
-    return parser.parse_args()
-
-
-def get_config(config_file) -> Dict[str, Any]:
-    """
-    Return the configuration file data
-    """
-    with open(config_file, 'r') as f:
-        return yaml.load(f, yaml.SafeLoader)
-
-
-def validate_config(config_data: Dict[str, Any]):
-    """
-    :rases: InvalidConfigException
-    """
-    required_keys = ['slug', 'email', 'api_key']
-    if missing := [key for key in required_keys if not config_data.get(key)]:
-        raise InvalidConfigException('Required configuration key(s) not set: ' + ', '.join(missing))
-
-
-def main():
-    args = get_args()
-    try:
-        config = get_config(args.config)
-        validate_config(config)
-    except FileNotFoundError:
-        print(f'Config file {args.config} not found, please specify --config/-c')
-        sys.exit(1)
-    except IOError as e:
-        print(f'Cannot open config file: {e}')
-        sys.exit(2)
-    except InvalidConfigException as e:
-        print(e)
-        sys.exit(3)
-
-    e2fa = Expenses2Fakturoid(config, args.supplier, args.filename)
-    try:
-        url = e2fa.run()
-    except HTTPError as e:
-        print(f'Error connecting to Fakturoid: {e}')
-        sys.exit(4)
-    except (FileNotFoundError, IOError) as e:
-        print(f'Cannot open input file: {e}')
-        print(repr(e))
-        sys.exit(5)
-    except (InvalidConfigException, ParseError) as e:
-        print(e)
-        sys.exit(6)
-
-    print(f'Expense imported successfully: {url}')
-
-
-if __name__ == '__main__':
-    main()
